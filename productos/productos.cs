96ee781e5418b8da83e1ecfc811c592eb6d9763b
Angel Alberto Garcia Guerrero <garciaguerreroaa18@gmail.com>
2018-03-13 23:10:54 -0600

    se agrego un formulario para productos

---
+++ productos/productos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace finall
+{
+    public partial class productos : Form
+    {
+        public productos()
+        {
+            InitializeComponent();
+        }
+
+        private void productosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productosBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.postgresDataSet1);
+
+        }
+
+        private void productos_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'postgresDataSet1.productos' Puede moverla o quitarla según sea necesario.
+            this.productosTableAdapter.Fill(this.postgresDataSet1.productos);
+
+        }
+    }
+}
